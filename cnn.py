cdedcf43ddc6b74b369d72ee98e9f6946bf5d32f
Morteza Mahdavi <98619035+MortezaMahdaviMortazavi@users.noreply.github.com>
2023-06-16 17:01:51 +0430

    Morteza Final Commit

--- cnn.py
+++ cnn.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+
 def MaxPooling2D(input_array, window, stride):
     input_height, input_width, input_channels = input_array.shape
 
@@ -24,6 +25,7 @@ def MaxPooling2D(input_array, window, stride):
                 output_array[j, k, i] = np.max(input_array[slice_start_h:slice_end_h, slice_start_w:slice_end_w, i])
 
     return output_array
+
 
 def Conv2D(img, filters, stride, padding):
     assert padding in ['same', 'valid']
@@ -87,7 +89,7 @@ def plot_channels(output):
         axes[idx].imshow(output[:,:,idx],cmap='gray')
         axes[idx].axis('off')
 
-    # axes[channels].imshow(output[:,:,:])
+    axes[channels].imshow(output[:,:,:])
     plt.tight_layout()
     plt.show()
 
@@ -119,98 +121,98 @@ def plot_outputs(outputs):
     plt.tight_layout()
     plt.show()
 
-if __name__ == "__main__":
-    bengio_path = 'images/bengio.jpg'
-    goodfellow_path = 'images/goodfellow.png'
-    hinton_path = 'images/hinton.jpg'
-    lecun_path = 'images/lecun.jpg'
+# if __name__ == "__main__":
+    # bengio_path = 'images/bengio.jpg'
+    # goodfellow_path = 'images/goodfellow.png'
+    # hinton_path = 'images/hinton.jpg'
+    # lecun_path = 'images/lecun.jpg'
 
-    bengio_img = np.array(Image.open(bengio_path))
-    goodfellow_img = np.array(Image.open(goodfellow_path))
-    hinton_img = np.array(Image.open(hinton_path))
-    lecun_img = np.array(Image.open(lecun_path))
+    # bengio_img = np.array(Image.open(bengio_path))
+    # goodfellow_img = np.array(Image.open(goodfellow_path))
+    # hinton_img = np.array(Image.open(hinton_path))
+    # lecun_img = np.array(Image.open(lecun_path))
 
-    """Layer 1"""
-    bottem_sobel = np.array([
-        [-1, -2, -1],
-        [0, 0, 0],
-        [1, 2, 1]]
-    )
+    # """Layer 1"""
+    # bottem_sobel = np.array([
+    #     [-1, -2, -1],
+    #     [0, 0, 0],
+    #     [1, 2, 1]]
+    # )
 
-    top_sobel = np.array([
-        [1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1]
-    ])
+    # top_sobel = np.array([
+    #     [1, 2, 1],
+    #     [0, 0, 0],
+    #     [-1, -2, -1]
+    # ])
 
-    sharpening = np.array([
-        [0, -1, 0],
-        [-1, 5, -1],
-        [0, -1, 0]
-    ])
+    # sharpening = np.array([
+    #     [0, -1, 0],
+    #     [-1, 5, -1],
+    #     [0, -1, 0]
+    # ])
 
-    weighted_averaging_3x3 = np.array([
-        [1, 2, 1],
-        [2, 4, 2],
-        [1, 2, 1]
-    ])
+    # weighted_averaging_3x3 = np.array([
+    #     [1, 2, 1],
+    #     [2, 4, 2],
+    #     [1, 2, 1]
+    # ])
 
-    """Layer 2"""
-    robert_x = np.array([
-        [1, 0],
-        [0, -1]
-    ])
+    # """Layer 2"""
+    # robert_x = np.array([
+    #     [1, 0],
+    #     [0, -1]
+    # ])
 
-    robert_y = np.array([
-        [0, 1],
-        [-1, 0]
-    ])
+    # robert_y = np.array([
+    #     [0, 1],
+    #     [-1, 0]
+    # ])
 
-    averaging_2x2 = np.array([
-        [1, 1],
-        [1, 1]
-    ])
+    # averaging_2x2 = np.array([
+    #     [1, 1],
+    #     [1, 1]
+    # ])
 
-    """Layer 3"""
-    gaussian_5x5 = np.array([
-        [1, 4, 7, 4, 1],
-        [4, 16, 26, 16, 4],
-        [7, 26, 41, 26, 7],
-        [4, 16, 26, 16, 4],
-        [1, 4, 7, 4, 1]
-    ])
+    # """Layer 3"""
+    # gaussian_5x5 = np.array([
+    #     [1, 4, 7, 4, 1],
+    #     [4, 16, 26, 16, 4],
+    #     [7, 26, 41, 26, 7],
+    #     [4, 16, 26, 16, 4],
+    #     [1, 4, 7, 4, 1]
+    # ])
 
-    averaging_5x5 = np.ones_like(gaussian_5x5) / 25
+    # averaging_5x5 = np.ones_like(gaussian_5x5) / 25
 
-    # Example usage with a grayscale image
-    l1_filters = []
-    l1_filters.append(bottem_sobel.reshape(3,3,1))
-    l1_filters.append(top_sobel.reshape(3,3,1))
-    l1_filters.append(sharpening.reshape(3,3,1))
+    # # Example usage with a grayscale image
+    # l1_filters = []
+    # l1_filters.append(bottem_sobel.reshape(3,3,1))
+    # l1_filters.append(top_sobel.reshape(3,3,1))
+    # l1_filters.append(sharpening.reshape(3,3,1))
 
-    l1_weighted_average = []
-    l1_weighted_average.append(weighted_averaging_3x3.reshape(3,3,1))
+    # l1_weighted_average = []
+    # l1_weighted_average.append(weighted_averaging_3x3.reshape(3,3,1))
 
-    l2_filters = []
-    l2_filters.append(robert_x.reshape(2,2,1))
-    l2_filters.append(robert_y.reshape(2,2,1))
-    l2_filters.append(averaging_2x2.reshape(2,2,1))
+    # l2_filters = []
+    # l2_filters.append(robert_x.reshape(2,2,1))
+    # l2_filters.append(robert_y.reshape(2,2,1))
+    # l2_filters.append(averaging_2x2.reshape(2,2,1))
 
-    l3_filters = []
-    l3_filters.append(gaussian_5x5.reshape(5,5,1))
-    l3_filters.append(averaging_5x5.reshape(5,5,1))
+    # l3_filters = []
+    # l3_filters.append(gaussian_5x5.reshape(5,5,1))
+    # l3_filters.append(averaging_5x5.reshape(5,5,1))
 
-    layers = [
-        {'filters':l1_filters,'stride':1,'padding':'same'},
-        {'filters':l1_weighted_average,"stride":1,'padding':'same'},
-        {'filters':l2_filters,"stride":2,'padding':'valid'},
-        {'filters':l3_filters,"stride":1,'padding':'same'},
-    ]
+    # layers = [
+    #     {'filters':l1_filters,'stride':1,'padding':'same'},
+    #     {'filters':l1_weighted_average,"stride":1,'padding':'same'},
+    #     {'filters':l2_filters,"stride":2,'padding':'valid'},
+    #     {'filters':l3_filters,"stride":1,'padding':'same'},
+    # ]
 
 
-    outputs = apply_conv_layers(bengio_img,layers)
-    last_output = outputs[3]
-    # print(last_output.shape)
-    plot_channels(last_output)
+    # outputs = apply_conv_layers(bengio_img,layers)
+    # last_output = outputs[0]
+    # # print(last_output.shape)
+    # plot_channels(last_output)
 
 
